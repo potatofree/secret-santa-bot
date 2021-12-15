0b6fed3be9772c0fcc3ba921b665b367b2091278
Aliaksandr Volkau <58636552+potatofree@users.noreply.github.com>
2021-12-15 17:15:16 +0100

    applied messageHandler

--- bot.js
+++ bot.js
@@ -7,13 +7,17 @@ const bot = new Bot(process.env.BOT_TOKEN)
 
 const welcomeMsg = `Hi! I'm Secret Santa bot. 
 Do you want to create a new Group for you and your friends?`;
-const keyboard =new InlineKeyboard()
+const keyboard = new InlineKeyboard()
   .text('Create Group', 'startCreationProcess')
   .text('Join Group', 'startJoiningProcess')
 
-let creationProcess = false;
-let joiningProcess = false;
 let groups = [];
+
+let basicMessageHandler = (ctx, next) => {
+  return next()
+};
+
+var messageHandler = basicMessageHandler;
 
 const checkGroupID = (ID) => {
   let check = false;
@@ -39,65 +43,55 @@ const addGroup = (group) => {
 
 const startCreationProcess = async (ctx, next) => {
   await ctx.reply('Nice! How do we name it?');
-  creationProcess = true;
+  messageHandler = createGroup;
   return next();
 }
 
 const createGroup = async (ctx, next) => {
-  if (creationProcess) {
-    let group = {};
-    group.name = ctx.message.text;
-    group.id = uniqId();
-    group.creator = ctx.chat.id;
-    group.userList= [ctx.chat.id];
-    creationProcess = false;
-    addGroup(group);
-    await ctx.reply('Cool name!');
-    await ctx.reply(`Group created.
+  let group = {
+    name: ctx.message.text,
+    id: uniqId(),
+    creator: ctx.chat.id,
+    userList: [ctx.chat.id],
+  };
+  addGroup(group);
+  await ctx.reply('Cool name!');
+  await ctx.reply(`Group created.
 Your group ID is:
 ${group.id}`);
-    ctx.reply(group.id);
-    return next();
-  } else {
-    return next()
-  }
+  ctx.reply(group.id);
+  messageHandler = basicMessageHandler;
+  return next();
 }
 const startJoiningProcess = (ctx) => {
   ctx.reply(`Sure, you can join group if you want.
 Just send me Group ID`);
-  joiningProcess = true;
+  messageHandler = joinGroup;
 };
 
 const joinGroup = (ctx, next) => {
-  if (joiningProcess) {
-    recivedID = ctx.message.text;
-    if (checkGroupID(recivedID)) {
-      addUserInGroup(recivedID, ctx.chat.id);
-      ctx.reply('You in!');
-      joiningProcess = false;
-      return next();
-    } else {
-      ctx.reply('I know nothing about group with that ID. =(');
-      joiningProcess = false;
-      return next();
-    }
+  recivedID = ctx.message.text;
+  if (checkGroupID(recivedID)) {
+    addUserInGroup(recivedID, ctx.chat.id);
+    ctx.reply('You in!');
   } else {
-    return next()
+    ctx.reply('I know nothing about group with that ID. =(');
   }
+  messageHandler = basicMessageHandler;
+  return next();
 };
 
-bot.command('start', async (ctx) => await ctx.reply(welcomeMsg,  { reply_markup: keyboard }))
+bot.command('start', async (ctx) => await ctx.reply(welcomeMsg, { reply_markup: keyboard }))
 
 bot.callbackQuery('startCreationProcess', startCreationProcess)
-bot.on('message', createGroup);
-
 bot.callbackQuery('startJoiningProcess', startJoiningProcess)
-bot.on('message', joinGroup);
+
+bot.on('message', (ctx, next) => { messageHandler(ctx, next) });
 
 const getUserGroupList = (id) => {
   let list = [];
-  groups.forEach( (group) => { 
-    if(group.userList.includes(id)) {
+  groups.forEach((group) => {
+    if (group.userList.includes(id)) {
       list.push(group);
     }
   })
@@ -107,7 +101,7 @@ const getUserGroupList = (id) => {
 const startEvent = (ctx, next) => {
   let groupList = getUserGroupList(ctx.chat.id);
   let groupNamesList = [];
-  groupList.forEach( (group) => {
+  groupList.forEach((group) => {
     let s = `${group.name} (${group.id}) ${group.status}`;
     groupNamesList.push(s);
   })
@@ -118,19 +112,18 @@ const startEvent = (ctx, next) => {
     startEventConfirmation(groupList[0]);
     return next();
   } else {
-  let s = 'Your groups: \n';
-  groupNamesList.forEach( (group, i) => {
-    s += `${i+1}: ${group} \n`
-  });
-  ctx.reply(s);
-  ctx.reply(`Which group do you want to start? (1-${groupNamesList.length})`);
+    let s = 'Your groups: \n';
+    groupNamesList.forEach((group, i) => {
+      s += `${i + 1}: ${group} \n`
+    });
+    ctx.reply(s);
+    ctx.reply(`Which group do you want to start? (1-${groupNamesList.length})`);
 
-  return next();
+    return next();
   };
-  // ctx.reply()
 
 }
-bot.command('startevent', startEvent); 
+bot.command('startevent', startEvent);
 
 //  
 bot.hears('groupList', (ctx) => (ctx.reply(groups)));
@@ -142,7 +135,7 @@ bot.start()
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
 
-// exports for tests
+// exports for the tests
 exports.startCreationProcess = startCreationProcess;
 exports.createGroup = createGroup;
 exports.checkGroupID = checkGroupID;
